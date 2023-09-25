4b1ccbd7600ea49bb2dd541832fb647146b5cb5c
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-25 22:23:12 +0800

    feat: create game session schema

---
+++ src/models/game-session.model.js
@@ -0,0 +1,31 @@
+const gameSessionSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: [true, "A game must have an ID"],
+    unique: true,
+  },
+  players: {
+    type: Array,
+    required: [true, "A game must have players"],
+    default: [null, null],
+  },
+  board: {
+    type: Array,
+    required: [true, "A game must have a gameboard"],
+    default: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  },
+  turn: {
+    type: Number,
+    required: [
+      true,
+      "Each round of a game must have a different player's turn",
+    ],
+  },
+  winner: {
+    type: String,
+  },
+});
+
+const GameSession = mongoose.model("GameSession", gameSessionSchema);
+
+export default GameSession;
