cb0b8d394a1416604b231961fc1978406af41eba
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 00:00:32 +0800

    feat: add join game session controller

--- src/controllers/game-session.controller.js
+++ src/controllers/game-session.controller.js
@@ -1,14 +1,6 @@
 import GameSession from "../models/game-session.model.js";
 import { v4 as uuidv4 } from "uuid";
 
-export const activeGameSessions = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message:
-      "This route should return all active game sessions in an array, but is not yet defined!",
-  });
-};
-
 export const checkId = (req, res, next, val) => {
   if (req.params.id === "1") {
     return res.status(400).json({
@@ -19,17 +11,47 @@ export const checkId = (req, res, next, val) => {
   next();
 };
 
-export const createGameSession = async (req, res) => {
+export const joinSession = async (req, res) => {
   try {
-    const id = uuidv4();
-    const newGameSession = await GameSession.create({ ...req.body, id });
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        gameSession: newGameSession,
-      },
+    const gameSession = await GameSession.findOne({
+      players: { $in: [null] },
     });
+
+    if (!gameSession) {
+      try {
+        const newGameSession = await GameSession.create({ id: uuidv4() });
+        const newPlayers = [req.body.pid, null];
+        newGameSession.players = newPlayers;
+        await newGameSession.save();
+        return res.status(201).json({
+          status: "success",
+          data: {
+            gameSession: newGameSession,
+          },
+        });
+      } catch (err) {
+        res.status(400).json({
+          status: "fail",
+          message: err,
+        });
+      }
+    } else {
+      if (gameSession.players[0] === null) {
+        const newPlayers = [req.body.pid, null];
+        gameSession.players = newPlayers;
+      } else {
+        const newPlayers = [...gameSession.players];
+        newPlayers[1] = req.body.pid;
+        gameSession.players = newPlayers;
+      }
+      await gameSession.save();
+      res.status(200).json({
+        status: "success",
+        data: {
+          gameSession,
+        },
+      });
+    }
   } catch (err) {
     res.status(400).json({
       status: "fail",
