25232cba3bbc14e1d1a12aa18625922f17d9c749
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-25 22:46:22 +0800

    feat: add create game session route

--- src/routes/game-session.routes.js
+++ src/routes/game-session.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { checkId } from "../controllers/game-session.controller.js";
+import {
+  checkId,
+  createGameSession,
+} from "../controllers/game-session.controller.js";
 
 const router = express.Router();
 
 router.param("id", checkId);
 
-router.route("/").get((req, res) => {
-  res.send("This is the game base router.");
-});
+router.route("/").post(createGameSession);
 
 export default router;
