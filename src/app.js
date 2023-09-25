09510adc26655802cada12ee5454962f66c75847
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-25 23:59:40 +0800

    feat: add body-parser to express app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import "dotenv/config";
 import express from "express";
 import gameSessionRouter from "./routes/game-session.routes.js";
 import playerRouter from "./routes/player.routes.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use("/game", gameSessionRouter);
 app.use("/player", playerRouter);
 
