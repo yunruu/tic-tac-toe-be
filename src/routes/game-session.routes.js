29f685ac9bfb4eb42faa010e028470294cb3eaec
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 00:00:41 +0800

    feat: add join game session route

--- src/routes/game-session.routes.js
+++ src/routes/game-session.routes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import {
   checkId,
-  createGameSession,
+  joinSession,
 } from "../controllers/game-session.controller.js";
 
 const router = express.Router();
 
 router.param("id", checkId);
 
-router.route("/").post(createGameSession);
+router.route("/").post(joinSession);
 
 export default router;
