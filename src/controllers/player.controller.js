18bd3717c1673ab24a67fcf2c31d9b323d473249
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-25 23:03:43 +0800

    fix: import player model file name bug

--- src/controllers/player.controller.js
+++ src/controllers/player.controller.js
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import Player from "../models/player.model";
+import Player from "../models/player.model.js";
 
 export const createPlayer = async (req, res) => {
   try {
