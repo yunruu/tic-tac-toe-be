684e02d78a7518c9989323de96b4aa29b83a27d8
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 10:34:50 +0800

    feat: add game session make move route

--- src/routes/game-session.routes.js
+++ src/routes/game-session.routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   joinSession,
   leaveSession,
+  makeMove,
 } from "../controllers/game-session.controller.js";
 
 const router = express.Router();
 
 router.route("/").post(joinSession);
 router.route("/:id/:pid").patch(leaveSession);
+router.route("/board/:id/:pid").put(makeMove);
 
 export default router;
