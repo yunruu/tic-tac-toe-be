6658fc2d685a3e741d54539c1bec846dce92082d
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 13:07:19 +0800

    feat: use cors in app

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import express from "express";
 import gameSessionRouter from "./routes/game-session.routes.js";
 import playerRouter from "./routes/player.routes.js";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/game", gameSessionRouter);
 app.use("/player", playerRouter);
 
